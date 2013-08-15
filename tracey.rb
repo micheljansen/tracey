b09399e48dee4832c5b2069ea099b72239eda99d
Michel Jansen <mail@micheljansen.org>
2013-08-15 14:56:59 +0100

    initial commit

---
+++ tracey.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+require "bundler"
+require "kramdown"
+require "erb"
+require "sinatra"
+require "sinatra/reloader"
+
+get "/" do
+
+  sections = []
+
+  dirs = Dir[File.join('public','content', '**/**')].select do |path|
+    File.directory?(path)
+  end
+  dirs.each do |dir|
+    section = {}
+    section[:path] = dir
+    section[:annotations] = "no annotations"
+
+    imgs = Dir.glob(File.join(dir,"*.{jpg,jpeg,svg}"), File::FNM_CASEFOLD)
+    puts imgs
+    section[:img] = imgs[0].gsub(/public/,"") if imgs.length > 0
+
+    begin
+      doc = Kramdown::Document.new(File.read(File.join(dir, "annotations.txt")))
+      puts "FOUND ANNOTATIONS: #{doc}"
+      section[:annotations] = doc.to_html
+    rescue => e
+      puts "#{dir}: no annotations"
+      p e
+    end
+    sections << section
+  end
+
+  p sections
+  erb :index, :locals  => {:sections => sections}
+end

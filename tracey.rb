a0b51f9f5ddf29e1100deb33f12474b44460c33a
Michel Jansen <mail@micheljansen.org>
2013-08-16 15:34:57 +0100

    naive attempt at sorting numerically

--- tracey.rb
+++ tracey.rb
@@ -11,9 +11,24 @@ get "/" do
 
   sections = []
 
-  dirs = Dir[File.join('public','content', '**/**')].select do |path|
+  base_path = File.join('public','content')
+
+  dirs = Dir[File.join(base_path, '**/**')].select do |path|
     File.directory?(path)
+  end.sort_by do |n|
+    # if the name starts with a number, sort it numerically
+    barename = n.gsub(Regexp.compile(base_path+"\/"), "")
+    match =/(^\d+)?(.*)/.match(barename).to_a
+    match.shift
+    if match.first.nil?
+      match.shift
+    else
+      match[0] = match[0].to_i
+    end
+    match
   end
+
+
   dirs.each do |dir|
     section = {}
     section[:path] = dir
